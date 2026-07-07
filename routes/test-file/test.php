459c91949d131568c4b9bae86056d0e7dc605e07
Hamza Abu Qwaider <h.abuqwaider@syarah.com>
2026-07-07 09:19:28 +0300

    add test file

---
+++ routes/test-file/test.php
@@ -0,0 +1,7 @@
+<!--curl -L \-->
+<!---X POST \-->
+<!---H "Accept: application/vnd.github+json" \-->
+<!---H "Authorization: Bearer YOUR_GITHUB_TOKEN" \-->
+<!---H "X-GitHub-Api-Version: 2026-03-10" \-->
+<!--https://api.github.com/repos/Hamzaabuqwaider/test-cicd/dispatches \-->
+<!---d '{"event_type":"custom-event"}'-->
